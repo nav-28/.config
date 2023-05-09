4a5de33e9dd985316d81da40158fead14613c565
nav-28 <pwadhwa@ualberta.ca>
2023-05-09 01:50:25 -0600

    test

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
+      "stevearc/dressing.nvim",
     },
     init = function()
       require("telescope").load_extension("flutter")

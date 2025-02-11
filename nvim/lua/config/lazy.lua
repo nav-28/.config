66f0506504a5434dcc3a5c20764fd663de0fa190
nav-28 <pwadhwa@ualberta.ca>
2025-02-10 23:32:07 -0700

    Add aerospace config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,6 +18,44 @@ return {
     },
   },
   {
+    "scalameta/nvim-metals",
+    ft = { "scala", "sbt", "java" },
+    -- stylua: ignore
+    keys = {
+      {'<leader>me', function() require "telescope".extensions.metals.commands() end, desc="Metals commands"},
+      {'<leader>mc', function() require "metals".compile_cascade() end, desc="Metals compile cascade"},
+    },
+    config = function()
+      local metals = require("metals")
+      local config = metals.bare_config()
+
+      config.init_options.statusBarProvider = "on"
+      config.settings = {
+        showImplicitArguments = true,
+        --excludedPackages = {  },
+        serverProperties = { "-Xmx2g" },
+        serverVersion = "latest.snapshot",
+      }
+
+      config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      config.on_attach = function(client, bufnr)
+        -- metals.setup_dap()
+        require("lsp-format").on_attach(client, bufnr)
+      end
+
+      -- Autocmd that will actually be in charge of starting the whole thing
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          metals.initialize_or_attach(config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
